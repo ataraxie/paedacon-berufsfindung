e85bbe49b8d7e0889b8981a29fccc68b72c5dc3d
Felix Grund <felix.grund@scandio.de>
2018-12-09 17:52:40 +0100

    Added background color mechanism

--- js/resume.js
+++ js/resume.js
@@ -1,12 +1,43 @@
 (function($) {
 	"use strict"; // Start of use strict
 
+	var $body = $(document.body);
+
+	var mapping = {
+		overview: "Startseite",
+		background: "Hintergrund",
+		offer: "MeinAngebot",
+		about: "UeberMich",
+		councelling: "Einzelberatung",
+		workshops: "Workshops",
+		contact: "Kontakt"
+	};
+
+	function setBackground(target) {
+		var id = $(target).attr("id");
+		var paeId = mapping[id];
+		var lastPaeField, backgroundSet = false;
+		if (paeId) {
+			if (PAE[paeId]) {
+				lastPaeField = PAE[paeId][PAE[paeId].length-1];
+				if (lastPaeField.startsWith("#")) {
+					$body.css("background-color", lastPaeField);
+					backgroundSet = true;
+				}
+			}
+		}
+		if (!backgroundSet) {
+			$body.css("background-color", "#fff");
+		}
+	}
+
 	// Smooth scrolling using jQuery easing
 	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
+				setBackground(target);
 				$('html, body').animate({
 					scrollTop: (target.offset().top)
 				}, 1000, "easeInOutExpo");
