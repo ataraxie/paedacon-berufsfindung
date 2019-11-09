248f331849bd30075ab5e641259cab766cb08a3a
Felix Grund <felix.grund@scandio.de>
2019-11-09 20:01:27 +0100

    Gulp update and burger menu style fix

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('css:compile', function() {
 			outputStyle: 'expanded'
 		}).on('error', sass.logError))
 		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
+			Browserslist: ['last 2 versions'],
 			cascade: false
 		}))
 		.pipe(header(banner, {
@@ -71,7 +71,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', gulp.series('css:compile'), function() {
 	return gulp.src([
 		'./css/*.css',
 		'!./css/*.min.css'
@@ -85,7 +85,7 @@ gulp.task('css:minify', ['css:compile'], function() {
 });
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', gulp.series('css:compile', 'css:minify'));
 
 // Minify JavaScript
 gulp.task('js:minify', function() {
@@ -105,10 +105,10 @@ gulp.task('js:minify', function() {
 });
 
 // JS
-gulp.task('js', ['js:minify']);
+gulp.task('js', gulp.series('js:minify'));
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', gulp.series('css', 'js', 'vendor'));
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -120,7 +120,7 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+gulp.task('dev', gulp.series('css', 'js', 'browserSync'), function() {
 	gulp.watch('./scss/*.scss', ['css']);
 	gulp.watch('./js/*.js', ['js']);
 	gulp.watch('./*.html', browserSync.reload);
